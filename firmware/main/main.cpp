b5a5ddab01f2c197c488d916e28e4eabbe6084b4
rek5io <janjuskowiak2004@gmail.com>
2026-05-09 00:46:28 +0200

    refactor: change repo structure

--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -11,7 +11,6 @@
 using namespace result;
 
 #define time 1000
-#define LED_GPIO GPIO_NUM_8
 
 struct Measurements {
     bmp280::Measurement bmp;
@@ -20,24 +19,24 @@ struct Measurements {
 
 mutex::Mutex<Measurements> mea = mutex::Mutex<Measurements>::init(Measurements());
 
-auto led_blink() -> void {
-    gpio_reset_pin(LED_GPIO);
-    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+auto led_task() -> void {
+    gpio_reset_pin(GPIO_NUM_8);
+    gpio_set_direction(GPIO_NUM_8, GPIO_MODE_OUTPUT);
 
     while (1) {
         //std::println("led on");
-        gpio_set_level(LED_GPIO, 0);
+        gpio_set_level(GPIO_NUM_8, 0);
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
         //std::println("led off");
-        gpio_set_level(LED_GPIO, 1);
+        gpio_set_level(GPIO_NUM_8, 1);
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 }
 
-auto bmp(i2c::I2cBus bus) -> void {
+auto sensors_task(i2c::I2cBus bus) -> void { 
     auto dev_result = i2c::I2cDevice::init(bus, 0x76);
     if (dev_result.is_err()) {
-        std::println("i2c bus init error");
+        std::println("bmp280 i2c dev init error");
         return;
     }
 
@@ -50,35 +49,27 @@ auto bmp(i2c::I2cBus bus) -> void {
     }
     
     auto bmp = bmp_result.unwrap();
+    auto dh = dht22::Dht22::from_gpio(GPIO_NUM_9);
 
     while (1) {
-        auto m = bmp.measure(); 
-        m.on_ok([](bmp280::Measurement m) {
+        bmp.measure().on_ok([](bmp280::Measurement m) {
             auto guard = mea.lock();
             guard.get_ref().bmp = m;
         });
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(time));
-    }
-}
-
-auto dht() -> void {
-    auto dh = dht22::Dht22::from_gpio(GPIO_NUM_9);
-
-    while (1) {
         dh.measure().on_ok([](dht22::Measurement m) {
             auto guard = mea.lock();
             guard.get_ref().dht = m;
         });
-        
-        std::this_thread::sleep_for(std::chrono::milliseconds(time));
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(125));
     }
 }
 
 auto oled_task(i2c::I2cBus bus) -> void {
     auto dev_result = i2c::I2cDevice::init(bus, 0x3c);
     if (dev_result.is_err()) {
-        std::println("i2c oled bus init error");
+        std::println("oled i2c dev init error");
         return;
     }
 
@@ -153,9 +144,9 @@ extern "C" void app_main(void)  {
     }
 
     auto bus = bus_result.unwrap();
-    auto fut_led = std::async(std::launch::async, led_blink);
-    auto fut_bmp = std::async(std::launch::async, [&]() { bmp(bus); });
-    auto fut_dht = std::async(std::launch::async, dht);
+
+    auto fut_led = std::async(std::launch::async, led_task);
+    auto fut_sensors = std::async(std::launch::async, [&]() { sensors_task(bus); });
     auto fut_oled = std::async(std::launch::async, [&]() { oled_task(bus); });
 
     while (1) {
